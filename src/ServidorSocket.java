ec99f551c9dbbf639fa5adc897659fe651a1c4d1
Robinpro10 <robinalzalara@gmail.com>
2024-08-07 10:12:26 -0500

    se codifica la clase servidor

--- src/ServidorSocket.java
+++ src/ServidorSocket.java
@@ -1,19 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+import java.io.*;
+import java.net.*;
 
-/**
- *
- * @author uptc-
- */
 public class ServidorSocket {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int puerto = 12345; // Puerto en el que el servidor escuchará las conexiones
+
+        try (ServerSocket serverSocket = new ServerSocket(puerto)) {
+            System.out.println("Servidor escuchando en el puerto " + puerto);
+
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     DataInputStream in = new DataInputStream(socket.getInputStream());
+                     DataOutputStream out = new DataOutputStream(socket.getOutputStream())) {
+
+                    System.out.println("Cliente conectado");
+
+                    int[] numeros = new int[10];
+                    for (int i = 0; i < 10; i++) {
+                        numeros[i] = in.readInt();
+                    }
+
+                    int max = numeros[0];
+                    int min = numeros[0];
+                    int suma = 0;
+                    for (int num : numeros) {
+                        if (num > max) {
+                            max = num;
+                        }
+                        if (num < min) {
+                            min = num;
+                        }
+                        suma += num;
+                    }
+
+                    String resultado = String.format("El número mayor es %d, el número menor es %d, y la suma de todos los números digitados es %d", max, min, suma);
+                    out.writeUTF(resultado);
+
+                    System.out.println("Resultados enviados al cliente: " + resultado);
+                } catch (IOException e) {
+                    System.err.println("Error en la conexión con el cliente: " + e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al iniciar el servidor: " + e.getMessage());
+        }
     }
-    
 }
